a2126aabea9d738f9c0555068d35e5c2a91df0da
Yann Legendre <yann.legendre@essec.edu>
2021-04-26 16:37:02 +0200

    copy part 1"

--- 03-Cookbook-01/Lecture/app.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'task_repository'
-require_relative 'tasks_controller'
-require_relative 'router'
-
-repository = TaskRepository.new
-controller = TasksController.new(repository)
-router = Router.new(controller)
-router.run
