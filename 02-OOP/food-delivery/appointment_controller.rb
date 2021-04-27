eb159a06d50157c1929d83b8b0d968b8f801094b
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-04-27 11:55:54 +0200

    correct typo

--- 02-OOP/food-delivery/appointment_controller.rb
+++ 02-OOP/food-delivery/appointment_controller.rb
@@ -5,11 +5,11 @@ class AppointmentController
   end
 
   def create
-    doctor_id = ask_for("doctor")
-    date = ask_for("date")
+    doctor_id  = ask_for("doctor")
+    date       = ask_for("date")
     patient_id = ask_for("dpatient")
-    doctor = @doctor_repo.find(doctor_id)
-    patient = @patient_repo.find(patient_id)
-    Appointment.new(doctor:doctor,patient)
+    doctor     = @doctor_repo.find(doctor_id)
+    patient    = @patient_repo.find(patient_id)
+    Appointment.new(doctor: doctor, patient: patient)
   end
 end
