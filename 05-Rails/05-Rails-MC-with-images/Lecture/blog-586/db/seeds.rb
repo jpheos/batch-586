54d5fb9e5594d024054a7b8f858f1e8fab977ba2
Joseph BLANCHARD <jooo.blanchard@gmail.com>
2021-05-21 10:55:12 +0200

    add /05-Rails/05-Rails-MC-with-images/Lecture

---
+++ 05-Rails/05-Rails-MC-with-images/Lecture/blog-586/db/seeds.rb
@@ -0,0 +1,8 @@
+require "open-uri"
+
+Article.destroy_all
+
+file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
+article = Article.new(title: 'NES', body: "A great console")
+article.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+article.save
